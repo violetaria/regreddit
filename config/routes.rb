bc27a242fad11108cbed2c32f2023353d197f105
violetaria <terri.chu@outlook.com>
2015-10-29 14:46:15 -0400

    initial route decisions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,25 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+
+  ## Registration Routes
+  get "signup", to: "registrations#new"
+  post "signup", to: "registrations#create"
+
+## Session Routes
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "login", to: "sessions#destroy"
+
+## Post Routes
+  get "posts", to: "posts#index"
+  get "posts/new", to "posts#new"
+  post "posts", to: "posts#create"
+
+## Comments Routes
+  get "posts/:id/comments", to: "comments#show"
+  post "posts/:id/comments", to: "comments#create"
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
