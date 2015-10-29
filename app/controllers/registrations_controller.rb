578ab3007de78b8bef0221918a0dbbec0e86db73
violetaria <terri.chu@outlook.com>
2015-10-29 16:37:57 -0400

    got basic user add working, added validations to user, email, password to model, basic root route put as a placeholder

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,19 @@
 class RegistrationsController < ApplicationController
 
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.create(username: params[:username],
+                email: params[:email],
+                password: params[:password])
+
+    binding.pry
+    redirect_to root_path
+  end
+
+  def index
+    render :index
+  end
 end
