fcae22a0b40f62e87dbabd229c0779113c49c393
Don Deckard <don_deckard@yahoo.com>
2015-10-30 11:47:18 -0400

    modified a tons of stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029190636) do
+ActiveRecord::Schema.define(version: 20151030141214) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
