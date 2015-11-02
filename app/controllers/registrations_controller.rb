849c08ab4ee7ec33d92d6ec38eb8e7a16b0b3633
violetaria <terri.chu@outlook.com>
2015-11-02 16:27:34 -0500

    changed registration so it automatically created a new session for that user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,6 +11,7 @@ class RegistrationsController < ApplicationController
                      password: params[:password])
     if @user.save
       flash[:notice] = "User #{params[:username]} created successfully"
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
