aceb6e40a33bfa22a33440d79e7b8cbfe7ecc4c4
violetaria <terri.chu@outlook.com>
2015-11-02 16:23:57 -0500

    fixed password check in the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by!(email: params[:email])
-    if user.password == params[:password]
+    if user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to posts_path
     else
