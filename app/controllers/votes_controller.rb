0509b079e3b880c51ca9a1e95caf5bb3932f4c29
Don Deckard <don_deckard@yahoo.com>
2015-11-01 23:37:32 -0500

    added more stuff

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,17 @@
+class VotesController < ApplicationController
+ ## TODO
+  before_action :authenticate_user!, only: [:create]
+
+  def up
+    @post = Post.find(params[:id])
+    @post.vote.create(user_id: current_user.id, post_id: @post.id vote: :vote +=1)
+    redirect_to post_path(@post)
+  end
+
+  def down
+    @post = Post.find(params[:id])
+    @post.vote.create(user_id: current_user.id, post_id: @post.id vote: :vote -=1)
+    redirect_to post_path(@post)
+  end
+
+end
