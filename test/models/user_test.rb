fd0aa9902f1818ad2d540bfb9272e3b39297167c
violetaria <terri.chu@outlook.com>
2015-11-10 16:30:26 -0500

    added easy peasy user test to add valid user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  ## TODO write one test
+  def test_can_create_valid_user
+    user = User.new(email: "user3",
+                    username: "userthree",
+                    password: "password")
+    assert user.valid?
+  end
 end
