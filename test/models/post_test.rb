60901114c78d75172d4d48f0aafce58e578d0f7a
violetaria <terri.chu@outlook.com>
2015-11-10 16:54:47 -0500

    added simple post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,11 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def test_new_valid_post
+    post = users(:userone).posts.new(url: "http://www.test.com",
+                                     title: "Test Title")
+
+    assert post.valid?
+  end
 end
