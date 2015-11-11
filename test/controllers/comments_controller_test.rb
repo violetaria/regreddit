ef7d00cd8f13571e7393ef67a76c5c4a7ff7b15a
violetaria <terri.chu@outlook.com>
2015-11-11 16:44:11 -0500

    added testing for delete, create, and show

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
 
-  test "can create comments" do
+  test "can create comments while logged in" do
     session[:user_id] = users(:one).id
     assert_difference "Comment.count" do
       post :create, { id: posts(:one).id, content: "Test content" }
@@ -15,7 +15,38 @@ class CommentsControllerTest < ActionController::TestCase
     assert_redirected_to comments_show_path(assigns(:post))
   end
 
-  test "can show comments" do
+  test "cannot create comments while not logged in" do
+    assert_no_difference "Comment.count" do
+      post :create, { id: posts(:one).id, content: "Test content" }
+    end
+
+    assert_response :found # 302
+
+    assert_redirected_to login_path
+  end
+
+  test "can show comments while not logged in" do
+    get :show, { id: posts(:one).id }
+
+    assert_response :ok # 200
+
+    assert_not_nil assigns(:post)
+
+    assert_not_nil assigns(:comments)
+  end
+
+  test "can delete their comments while logged in" do
+    session[:user_id] = users(:one).id
+
+    assert_difference "Comment.count", -1 do
+      delete :delete, { id: posts(:one).id, comment_id: comments(:one).id }
+    end
+
+    assert_response :found #302
+
+    assert_not_nil assigns(:post)
+
+    assert_redirected_to comments_show_path(assigns(:post))
 
   end
 
