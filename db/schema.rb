bd0eaa7126ed6cb44d3230ffea44de2921ca8ba1
violetaria <terri.chu@outlook.com>
2015-11-18 14:50:53 -0500

    updated migration to remove bcrypt column password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118025652) do
+ActiveRecord::Schema.define(version: 20151118193653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20151118025652) do
     t.string   "email",                              default: "", null: false
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
-    t.string   "password_digest"
     t.string   "encrypted_password",     limit: 128, default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
