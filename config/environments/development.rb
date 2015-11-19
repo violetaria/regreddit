4a7600169760d19c3b77707b340962ff1546e3ec
violetaria <terri.chu@outlook.com>
2015-11-19 08:45:28 -0500

    mandrill setup for all the envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,4 +41,16 @@ Rails.application.configure do
 
   # devise setup
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # ActionMailer setup to use Mandrill
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.mandrillapp.com',
+      port: 587,
+      user_name: ENV['MANDRILL_USERNAME'],
+      password: ENV["MANDRILL_API_KEY"],
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mandrill
-  set config.action_mailer.default_url_options = { host: 'smtp.mandrillapp.com', port: 587 }
+  set config.action_mailer.default_url_options = { host: 'pacific-hamlet-1123.', port: 443 }
+
+  # ActionMailer setup to use Mandrill
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.mandrillapp.com',
+      port: 587,
+      user_name: ENV['MANDRILL_USERNAME'],
+      password: ENV["MANDRILL_API_KEY"],
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 end
