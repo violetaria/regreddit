048f5118921b712d692c9c15efd60753e9800373
violetaria <terri.chu@outlook.com>
2015-11-22 12:43:51 -0500

    added protocol to config action mailer setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mandrill
-  config.action_mailer.default_url_options = { host: 'regreddit.herokuapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'regreddit.herokuapp.com', protocol: 'https' }
 
   # ActionMailer setup to use Mandrill
   config.action_mailer.delivery_method = :smtp
