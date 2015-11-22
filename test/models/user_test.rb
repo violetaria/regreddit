b54c3c87c188493d8398b22053c98abb75e0af03
violetaria <terri.chu@outlook.com>
2015-11-21 19:01:15 -0500

    remove user model tests as devise handles this now

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,30 +5,30 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  def test_new_valid_user
-    user = User.new(email: "user3@email.com",
-                    username: "userthree",
-                    password: "password123")
-    assert user.save
-    assert user.errors.empty?
-  end
-
-  def test_new_user_invalid_email
-    user = User.new(username: "userthree",
-                    password: "password")
-    refute user.valid?
-  end
-
-  def test_new_user_invalid_username
-    user = User.new(email: "user3@email.com",
-                    password: "password")
-    refute user.valid?
-  end
-
-  def test_new_user_invalid_password
-    user = User.new(username: "userthree",
-                    email: "user3@email.com")
-    refute user.valid?
-  end
+  # def test_new_valid_user
+  #   user = User.new(email: "user3@email.com",
+  #                   username: "userthree",
+  #                   password: "password123")
+  #   assert user.save
+  #   assert user.errors.empty?
+  # end
+  #
+  # def test_new_user_invalid_email
+  #   user = User.new(username: "userthree",
+  #                   password: "password")
+  #   refute user.valid?
+  # end
+  #
+  # def test_new_user_invalid_username
+  #   user = User.new(email: "user3@email.com",
+  #                   password: "password")
+  #   refute user.valid?
+  # end
+  #
+  # def test_new_user_invalid_password
+  #   user = User.new(username: "userthree",
+  #                   email: "user3@email.com")
+  #   refute user.valid?
+  # end
 
 end
